fb989348a98f48c77b4bb40c87c626f5c038fcb0
asougane <abdallah.sougane@unchk.edu.sn>
2025-08-26 18:37:24 +0000

    ajout de la redirection sur la methode update vers la liste des auteurs si la modification est correcte

--- src/app/pages/auteur/update-auteur/update-auteur.component.ts
+++ src/app/pages/auteur/update-auteur/update-auteur.component.ts
@@ -19,10 +19,19 @@ export class UpdateAuteurComponent {
   auteur: Auteur = {};
   dateError: boolean = false;
 
-  ngOnInit(): void {
-    this.recuperationAuteur();
+ngOnInit(): void {
 
-  const idAuteur = this.activatedRoute.snapshot.paramMap.get('id');
+  this.recuperationAuteur();
+
+}
+
+constructor(private toastr: ToastrService, private router: Router,
+    private auteurService: AuteurService,
+    private activatedRoute: ActivatedRoute) {}
+
+
+  recuperationAuteur(){
+    const idAuteur = this.activatedRoute.snapshot.paramMap.get('id');
   if (idAuteur) {
     const id = Number(idAuteur); // convertir en number
     this.auteurService.getAuteurById(id).subscribe({
@@ -34,20 +43,12 @@ export class UpdateAuteurComponent {
               ? new Date(auteur.dateNaissance).toISOString().substring(0, 10)
               : ''
           };
+
         }
       },
       error: (err) => console.error("Erreur lors de la récupération de l'auteur", err)
     });
   }
-}
-
-constructor(private toastr: ToastrService,
-    private auteurService: AuteurService,
-    private activatedRoute: ActivatedRoute) {}
-
-
-  recuperationAuteur(){
-
 }
 
 
@@ -69,7 +70,7 @@ checkPastDate(value: string) {
             console.log("Auteur modifier : ", response);
             // Affichage du toast après succès
             this.toastr.warning("L'auteur est modifié avec succès");
-
+            this.router.navigate(['listAuteur'])
             // Réinitialiser le formulaire et les champs
             form.resetForm();
             this.auteur = {};
